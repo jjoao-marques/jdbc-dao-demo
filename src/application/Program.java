fcab7620b53771bf18cbb88f8b68c4d1e9d78213
João Marques <joaomarques.jm991@gmail.com>
2021-04-17 23:50:34 -0300

    Reusing instantiation

--- src/application/Program.java
+++ src/application/Program.java
@@ -10,9 +10,12 @@ public class Program {
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
-		Seller seller = sellerDao.findById(3);
 		
+		System.out.println("=== TEST 1: seller findById ===");
+		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
+		
+		System.out.println("=== TEST 2: seller  ===");
 	}
 
 }
